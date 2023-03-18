14f46449abab228e7965f9f3c0935c9b41b8e5ba
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-03-17 21:33:40 -0300

    chore: add client interface

--- api/database/schemas/types/client.ts
+++ api/database/schemas/types/client.ts
@@ -3,10 +3,10 @@ import { Document } from 'mongoose'
 export interface ClientInterface extends Document {
   name: string
   cpf: string
-  address: Date
-  phone: Date
-  email: Date
-  birthDate: Boolean
+  address: string
+  phone: string
+  email: string
+  birthDate: Date
   hash: string
   salt: string
   refreshJWT: Object
