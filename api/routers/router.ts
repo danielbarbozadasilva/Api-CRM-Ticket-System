51ecd2794cc4dfcab032be0389045c8425a00782
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-03-19 16:23:23 -0300

    chore: add call routes

--- api/routers/router.ts
+++ api/routers/router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
 import routersV1Auth from './v1/routers.auth'
+import routersV1Call from './v1/routers.call'
 
 const router = Router()
 routersV1Auth(router)
+routersV1Call(router)
 
 export { router }
