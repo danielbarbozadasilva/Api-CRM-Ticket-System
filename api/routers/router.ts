aba6c6112dff5cfe68a47e3cca38b3670423a7d0
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-03-26 16:26:36 -0300

    chore: add client route

--- api/routers/router.ts
+++ api/routers/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import routersV1Auth from './v1/routers.auth'
 import routersV1Call from './v1/routers.call'
+import routersV1Client from './v1/routers.client'
 
 const router = Router()
 routersV1Auth(router)
 routersV1Call(router)
+routersV1Client(router)
 
 export { router }
