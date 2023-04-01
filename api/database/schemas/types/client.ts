358414d3b1759cb5ed1800e6e0b2d9ddf2a2e7d4
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2023-04-01 13:53:12 -0300

    chore: rename file

--- api/database/schemas/types/client.ts
+++ api/database/schemas/types/client.ts
@@ -1,6 +1,6 @@
 import { Document } from 'mongoose'
 
-export interface ClientInterface extends Document {
+export interface UserInterface extends Document {
   name: string
   cpf: string
   address: string
